2e9ceeacfdb07204d9cd39a9a2ad46e6c1fcefea
KamiGasai <36142755+KamiGasai@users.noreply.github.com>
2018-04-02 05:28:07 -0700

    Add files via upload
    sys1.java is same as Simulator.java (with a little changes), it is a branch prediction simulator with static prediction. (Forward branches are predicted not taken, backward branches predicted taken)
    
    sys2.java is based on sys1, it is also a branch prediction simulator (basic 2-bit predictor with branch target buffer).
    (N-entry 2-bit predictor/M-entry branch target buffer)
    
    Using traces for SPEC 2006 benchmarks, from University of Pennsylvania:
    https://www.cis.upenn.edu/~milom/cis501-Fall12/traces/trace-format.html

---
+++ sys1.java
@@ -0,0 +1,127 @@
+import java.lang.Exception;
+import java.lang.Long;
+import java.lang.String;
+import java.lang.System;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.Reader;
+
+class sys1
+{
+    
+    public static void simulate(InputStream incomingStream, PrintStream outputStream) throws Exception 
+    {
+        // See the documentation to understand what these variables mean.
+        long microOpCount; // 1
+        long instructionAddress;// 2
+        long sourceRegister1; // 3
+        long sourceRegister2; // 4
+        long destinationRegister; // 5
+        String conditionRegister; // 6
+        String TNnotBranch; // 7
+        String loadStore; // 8
+        long immediate; // 9
+        long addressForMemoryOp; // 10
+        long fallthroughPC; // 11
+        long targetAddressTakenBranch; // 12 
+        String macroOperation; // 13 
+        String microOperation; // 14
+
+        long totalMicroops = 0; 
+        long totalMacroops = 0;
+        int NOB = 0; // numbers of branches
+ 	int FB = 0; // Forward branches
+	int FTB = 0; // Forward taken branches
+	int BB = 0;  // Backward branches
+	int BTB = 0; // Backward taken branches
+	int MP = 0; // Number of mispredictions
+
+        BufferedReader r = new BufferedReader(new InputStreamReader(incomingStream));
+        String line;
+        
+//        outputStream.format("Processing trace...\n");
+        
+        while (true) {
+            line = r.readLine();
+            if (line == null) {
+                break;
+            }
+            String [] tokens = line.split("\\s+");
+            
+            microOpCount = Long.parseLong(tokens[0]);
+            instructionAddress = Long.parseLong(tokens[1], 16);
+            sourceRegister1 = Long.parseLong(tokens[2]);
+            sourceRegister2 = Long.parseLong(tokens[3]);
+            destinationRegister = Long.parseLong(tokens[4]);
+            conditionRegister = tokens[5];
+            TNnotBranch = tokens[6];
+            loadStore = tokens[7];
+            immediate = Long.parseLong(tokens[8]);
+            addressForMemoryOp = Long.parseLong(tokens[9], 16);
+            fallthroughPC = Long.parseLong(tokens[10], 16);
+            targetAddressTakenBranch = Long.parseLong(tokens[11], 16);
+            macroOperation = tokens[12];
+            microOperation = tokens[13];
+            
+            // For each micro-op
+            totalMicroops++;
+
+            // For each macro-op
+            if (microOpCount == 1) {
+                totalMacroops++;
+            }
+
+	    //System.out.println(TNnotBranch);
+	    //System.out.println(instructionAddress);
+            if (TNnotBranch.equals("N") || TNnotBranch.equals("T")) { 
+		NOB++;
+		if (instructionAddress < targetAddressTakenBranch) { 
+			FB++;
+			if (TNnotBranch.equals("T")) { FTB++;MP++;}
+		} 
+		else { 
+			BB++;
+			if (TNnotBranch.equals("T")) { BTB++;}
+			else { MP++;}
+		}
+
+	    }
+        }
+        
+//        outputStream.format("Processed %d trace records.\n", totalMicroops);
+        
+  //      outputStream.format("Micro-ops: %d\n", totalMicroops);
+    //    outputStream.format("Macro-ops: %d\n", totalMacroops);
+	System.out.println("Number of branches = " + NOB 
+			    + "\nNumber of forward branches = " + FB
+			    + "\nNumber of forward taken branches = " + FTB
+			    + "\nNumber of backward branches = " + BB
+			    + "\nNumber of backward taken branches = " + BTB
+			    + "\nNumber of misprediction = " + MP
+			    + "\nMisprediction rate = " + ((double)MP/(NOB)));
+    }
+    
+    public static void main(String[] args) throws Exception
+    {
+        InputStream inputStream = System.in;
+        PrintStream outputStream = System.out;
+        
+        if (args.length >= 1) {
+            inputStream = new FileInputStream(args[0]);
+        }
+        
+        if (args.length >= 2) {
+            outputStream = new PrintStream(args[1]);
+        }
+        
+        sys1.simulate(inputStream, outputStream);
+	//long test = Long.parseLong("aa",16);
+	//System.out.println(Long.toString(test,2));
+	
+    }
+}
---
+++ sys2.java
@@ -0,0 +1,214 @@
+import java.math.BigInteger;
+import java.lang.Exception;
+import java.lang.Long;
+import java.lang.String;
+import java.lang.System;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.Reader;
+
+class sys2
+{
+    
+    public static void simulate(InputStream incomingStream, int numPB, int numBTB, int verboseMode) throws Exception 
+    {
+        // See the documentation to understand what these variables mean.
+        long microOpCount; // 1
+        long instructionAddress;// 2
+        long sourceRegister1; // 3
+        long sourceRegister2; // 4
+        long destinationRegister; // 5
+        String conditionRegister; // 6
+        String TNnotBranch; // 7
+        String loadStore; // 8
+        long immediate; // 9
+        long addressForMemoryOp; // 10
+        long fallthroughPC; // 11
+        long targetAddressTakenBranch; // 12 
+        String macroOperation; // 13 
+        String microOperation; // 14
+
+        long totalMicroops = 0; 
+        long totalMacroops = 0;
+        int NOB = 0; // numbers of branches
+ 	int FB = 0; // Forward branches
+	int FTB = 0; // Forward taken branches
+	int BB = 0;  // Backward branches
+	int BTB = 0; // Backward taken branches
+	int MP = 0; // Number of mispredictions
+ 	int missedBTB = 0; //This is numer of "missed Branch Buffer Target"
+	int missedBTBRate = 0;
+	//SYS2
+	
+	int PBbits = (int)(Math.log(numPB)/Math.log(2));
+	int BTBbits = (int)(Math.log(numBTB)/Math.log(2));
+
+        int PBuffer[] = new int [numPB];
+	int BTBuffer[] = new int [numBTB];
+	String BTBtag [] = new String [numBTB];
+	for (int i = 0; i < numPB; i++) {
+	    PBuffer[i] = 1;
+	}		
+	for (int j = 0; j < numBTB;j++) {
+	    BTBuffer[j] = 0;
+	    BTBtag[j] = "";
+	}
+	int BTBhit = 0;
+	int BTBmiss = 0;
+	
+        BufferedReader r = new BufferedReader(new InputStreamReader(incomingStream));
+        String line;
+        
+     
+        
+        while (true) {
+            line = r.readLine();
+            if (line == null) {
+                break;
+            }
+            String [] tokens = line.split("\\s+");
+            
+            microOpCount = Long.parseLong(tokens[0]);
+            instructionAddress = Long.parseLong(tokens[1], 16);
+            sourceRegister1 = Long.parseLong(tokens[2]);
+            sourceRegister2 = Long.parseLong(tokens[3]);
+            destinationRegister = Long.parseLong(tokens[4]);
+            conditionRegister = tokens[5];
+            TNnotBranch = tokens[6];
+            loadStore = tokens[7];
+            immediate = Long.parseLong(tokens[8]);
+            addressForMemoryOp = Long.parseLong(tokens[9], 16);
+            fallthroughPC = Long.parseLong(tokens[10], 16);
+            targetAddressTakenBranch = Long.parseLong(tokens[11], 16);
+            macroOperation = tokens[12];
+            microOperation = tokens[13];
+            
+            // For each micro-op
+            totalMicroops++;
+
+            // For each macro-op
+            if (microOpCount == 1) {
+                totalMacroops++;
+            }
+
+	    //System.out.println(TNnotBranch);
+	    //System.out.println(instructionAddress);
+            if (TNnotBranch.equals("N") || TNnotBranch.equals("T")) { 
+		String BinaryFullStr =  Long.toBinaryString(instructionAddress);
+		String BinaryPBStr = BinaryFullStr.substring(BinaryFullStr.length() - PBbits, BinaryFullStr.length());
+		String BinaryBTBStr = BinaryFullStr.substring(BinaryFullStr.length() - BTBbits, BinaryFullStr.length());
+		String BinaryTag = BinaryFullStr.substring(0, BinaryFullStr.length() - BTBbits);
+		BigInteger bi = new BigInteger(BinaryTag, 2);
+		String Taghex = bi.toString(16);
+		int PBindex = Integer.parseInt(BinaryPBStr,2);	
+		int BTBindex = Integer.parseInt(BinaryBTBStr,2);
+		boolean taken = false;
+		boolean predictTaken = false;
+
+		//System.out.println(BinaryFullStr + "\n" + BinaryPBStr + "\n" + BinaryBTBStr +"\n")
+		if (verboseMode == 1) {;
+			System.out.print(NOB + " " + PBindex + " " + PBuffer[PBindex] + " " );
+		}
+		if (BTBtag[BTBindex].length() == 0) {BTBtag[BTBindex] = Taghex;}
+		
+		if (PBuffer[PBindex] >=2) {
+		    predictTaken = true;
+		}
+
+		if (predictTaken) {
+		    if (BTBtag[BTBindex].equals(Taghex)) {
+		    	BTBhit++;
+			if (BTBuffer[BTBindex] == 0) {BTBmiss++; BTBuffer[BTBindex] = 1;}//System.out.println("missed");
+		    }
+		}
+		
+		if (instructionAddress < targetAddressTakenBranch) { 
+			FB++;
+			if (TNnotBranch.equals("T")) { 
+			    FTB++;
+			    taken = true;
+	         	    if (PBuffer[PBindex] < 3) { PBuffer[PBindex]++; }
+			    
+			} 
+			else { if(PBuffer[PBindex] > 0) { PBuffer[PBindex]--; }}
+		} 
+		else { 
+			BB++;
+			//System.out.println("Backward Branch");
+			if (TNnotBranch.equals("T")) { 
+			    BTB++;
+			    taken = true;
+			    if (PBuffer[PBindex] < 3) { PBuffer[PBindex]++; }
+			}
+			else {
+			    if (PBuffer[PBindex] > 0) { PBuffer[PBindex]--; }
+			}
+		}
+		if (predictTaken != taken) { MP++;}
+		
+		if ((taken && predictTaken)) {
+		     BTBuffer[BTBindex] = 1;
+	             BTBtag[BTBindex] = Taghex;
+		}
+		NOB++;
+		if (verboseMode == 1) {
+	    	    System.out.println(PBuffer[PBindex] + " " + BTBindex + " " + Taghex + " " + BTBhit + " " + BTBmiss);
+	   	}
+	  	// System.out.println("Current hextag: " + BTBtag[BTBindex] + "Current VB: " + BTBuffer[BTBindex]);
+	   // System.out.println("--------------");
+           }
+	}
+        
+	System.out.println("Number of branches = " + NOB 
+			    + "\nNumber of forward branches = " + FB
+			    + "\nNumber of forward taken branches = " + FTB
+			    + "\nNumber of backward branches = " + BB
+			    + "\nNumber of backward taken branches = " + BTB
+			    + "\nNumber of misprediction = " + MP
+			    + "\nMisprediction rate = " + ((double)MP/(NOB))
+			    + "\nNumber of BTB miss = " + BTBmiss + " " + (double) BTBmiss / BTBhit);
+    }
+    
+    public static void main(String[] args) throws Exception
+
+    {
+        InputStream inputStream = System.in;
+//	inputStream = new FileInputStream(args[0]);
+	if (args.length >= 3) {
+       	    int NoePB = Integer.parseInt(args[1]);
+	    int NoeBTB = Integer.parseInt(args[2]);
+	  //  int numOfBitsPB;
+          //  int numOfBitsBTB;
+	    boolean check1 = NoePB > 0 && ((NoePB & (NoePB -1)) == 0);
+	    boolean check2 = NoeBTB > 0 && ((NoeBTB & (NoeBTB - 1)) == 0);
+    
+            inputStream = new FileInputStream(args[0]);
+	    if (check1 && check2){
+		inputStream = new FileInputStream(args[0]);
+		if (args.length == 4 && args[3].equals("-v")) {
+		    System.out.println("Verbose Mode On");
+		    sys2.simulate(inputStream, NoePB, NoeBTB, 1);
+		}else {
+                //numOfBitsPB = (int) (Math.log(NoePB)/Math.log(2));
+		//numOfBitsBTB = (int) (Math.log(NoeBTB)/Math.log(2));
+		    System.out.println("Verbose Mode Off, use \"java sys xxx.trace N M -v \"to enable Verbose Mode to see the lists of integer for simulation");
+		    sys2.simulate(inputStream,NoePB, NoeBTB, 0);
+		}		
+	    } else {
+	        System.out.println("Numbers of entries of Predict Buffer and Branch Taken Buffer has to be power of 2");
+	    }
+        } 
+	 else {
+	    System.out.println("Error on entering parameter, please check and re-run the project");
+       }
+
+	//long test = Long.parseLong("aa",16);
+	//System.out.println(Long.toString(test,2));
+	
+   }
+}
